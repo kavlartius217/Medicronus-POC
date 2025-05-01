8c0a5ea40257b7d749b03415d473bb6832a0585a
Kavin Lartius <kavlartius@gmail.com>
2025-05-01 13:32:44 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 # app.py
 # Consolidated script for MediCronus POC
-
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
 import streamlit as st
 import os
 import uuid # To create unique filenames for uploads
